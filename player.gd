25ba1fb8c0110ac77b11e1829d5d4fa62f45c578
Authomas2000 <89857938+Authomas2000@users.noreply.github.com>
2024-09-24 02:47:51 -0400

    Camera Logic
    Added picture taking, and preview canvas layer

--- player.gd
+++ player.gd
@@ -1,19 +1,58 @@
 extends CharacterBody3D
 
-
-const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
 @onready var pivot = $CamOrigin
 @export var sens = 0.5
+@export var CameraViewport: SubViewport # Reference to the Camera's SubViewport
+@export var CameraPreview: TextureRect    # Reference to the TextureRect
+const SPEED = 5.0
+const JUMP_VELOCITY = 4.5
+var pictureCount = 1
+var pictureCapacity = 5
 
 func _ready():
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	
+	CameraPreview.texture = CameraViewport.get_texture()
+	
+	setupCamera()
+	
+func setupCamera():
+	var dir = DirAccess.open("user://")
+	dir.make_dir("pictures")
+	
+	dir = DirAccess.open("user://pictures")
+	
+	
+	for n in dir.get_files():
+		pictureCount += 1
+
+func takePicture():
+	if pictureCount != (pictureCapacity + 1):
+		# Wait for the frame to be fully rendered
+		await RenderingServer.frame_post_draw
+		
+		if CameraViewport and CameraViewport.get_texture():
+			# Get the image from the camera's SubViewport instead of the default viewport
+			var img = CameraViewport.get_texture().get_image()
+			
+			if img:
+				img.save_png("user://pictures/picture"+str(pictureCount)+".png")
+				pictureCount += 1
+				
+			else:
+				print("Failed to capture the image from the SubViewport.")
+		else:
+			print("SubViewport or its texture is not available.")
+
 
 func _input(event):
 	if event is InputEventMouseMotion:
 		rotate_y(deg_to_rad(-event.relative.x * sens))
 		pivot.rotate_x(deg_to_rad(-event.relative.y * sens))
 		pivot.rotation.x = clamp(pivot.rotation.x, deg_to_rad(-90), deg_to_rad(45))
+		
+	if event.is_action_pressed("TakePicture"):
+		takePicture()
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
